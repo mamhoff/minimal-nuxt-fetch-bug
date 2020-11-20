ac299ab10c277843427c38da8ecaffe704c7ad01
Martin Meyerhoff <mamhoff@gmail.com>
2020-11-20 21:34:43 +0100

    Add Post

---
+++ components/Post.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    body: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
